876a606b0debd82e8b5c030990a5bdaff1ac834d
charmeee <mandugohigh@gmail.com>
2023-01-26 02:14:05 +0900

    style: apply prettier

--- src/services/auth_api.js
+++ src/services/auth_api.js
@@ -1,17 +1,17 @@
-import client from "./http_client";
+import client from './http_client';
 
 class Auth {
-  login = async (email, password) => {
-    try {
-      const user = await client.post("member/login", {
-        email: email,
-        password: password,
-      });
-      return user;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    login = async (email, password) => {
+        try {
+            const user = await client.post('member/login', {
+                email: email,
+                password: password,
+            });
+            return user;
+        } catch (error) {
+            console.log(error);
+        }
+    };
 }
 
 const authService = new Auth();
