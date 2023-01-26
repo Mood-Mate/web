4dc20e654dec59d5f58570645c8774cdafdc1df9
charmeee <mandugohigh@gmail.com>
2023-01-26 19:48:37 +0900

    feat: add interceptor frame

--- src/services/http_client.js
+++ src/services/http_client.js
@@ -1,11 +1,36 @@
-import axios from "axios";
-import secure from "../secure/secure";
+import axios from 'axios';
+import secure from '../secure/secure';
+import cookie from 'react-cookies';
 
 let baseURL = secure.baseUrl;
 
 const httpClient = axios.create({
-  baseURL,
-  withCredentials: true,
+    baseURL,
+    timeout: 1000,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
 });
+
+httpClient.interceptors.request.use(
+    (config) => {
+        const token = cookie.load('access_token');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+            return config;
+        }
+    },
+    (error) => {
+        if (error.response) {
+            if (error.response.status === 401) {
+                // Do something, call refreshToken() request for example;
+                // return a request
+                //return axios_instance(config);
+                //refresh토큰으로 토큰 재요청 후 그것도 안되면 로그아웃.
+            }
+            return Promise.reject(error.response.data);
+        }
+        return Promise.reject(error);
+    },
+);
 
 export default httpClient;
