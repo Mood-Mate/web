29ee2ddef05d42a566c2ba826483b2fb3170afb2
charmeee <mandugohigh@gmail.com>
2023-01-27 01:42:19 +0900

    feat(api): 유저 정보 가져오는 api 추가

--- src/services/auth_api.js
+++ src/services/auth_api.js
@@ -1,15 +1,35 @@
 import client from './http_client';
+import cookie from 'react-cookies';
 
 class Auth {
     login = async (email, password) => {
         try {
-            const user = await client.post('member/login', {
+            const response = await client.post('member/login', {
                 email: email,
                 password: password,
             });
-            return user;
+            console.log(response);
+            return await this.getUser(response.data['accessToken']);
         } catch (error) {
             console.log(error);
+            return null;
+        }
+    };
+    getUser = async (token) => {
+        try {
+            const expires = new Date();
+            expires.setDate(expires.getDate() + 1); // 1일동안 쿠키 유지
+            cookie.save('access_token', token, {
+                path: '/',
+                expires,
+            });
+            const response = await client.post('member/auth');
+            console.log(response);
+
+            return response;
+        } catch (error) {
+            console.log(error);
+            return null;
         }
     };
 }
