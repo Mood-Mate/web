0d9508c50f73e31b4228d9e82dc765127fb903de
charmeee <mandugohigh@gmail.com>
2023-01-29 14:14:16 +0900

    fix: add router click profile => route to profile

--- src/components/Home/topProfile.js
+++ src/components/Home/topProfile.js
@@ -1,27 +1,29 @@
 import { Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export default function TopProfile() {
-	return (
-		<>
-			<Box
-				sx={{
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					alignItems: 'center',
-					textAlign: 'center',
-				}}
-			>
-				<Box
-					sx={{
-						width: 100,
-						height: 100,
-						borderRadius: '50%',
-						border: 2,
-						borderColor: 'primary.main',
-					}}
-				></Box>
-			</Box>
-		</>
-	);
+    return (
+        <>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                }}>
+                <Link to="/profile">
+                    <Box
+                        sx={{
+                            width: 100,
+                            height: 100,
+                            borderRadius: '50%',
+                            border: 2,
+                            borderColor: 'primary.main',
+                        }}
+                    />
+                </Link>
+            </Box>
+        </>
+    );
 }
