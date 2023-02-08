9b4e36b847544ee915caf288841c95c245864447
charmeee <mandugohigh@gmail.com>
2023-02-09 07:01:27 +0900

    feat: 댓글 api 추가 및 ui 에 반영

--- src/components/Profile/comments.js
+++ src/components/Profile/comments.js
@@ -1,44 +1,76 @@
 import { Avatar, Box, IconButton, InputBase, Stack } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import SendIcon from '@mui/icons-material/Send';
+import { useState } from 'react';
+import diaryService from '../../services/diary_api';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { diaryState } from '../../atom/dairy';
 
 export default function Comments(props) {
-    const comments = [
-        {
-            id: 1,
-            username: '앙버터',
-            content: '감명 깊게 봤습니다,,,',
-        },
-        {
-            id: 2,
-            username: '만두',
-            content: '대단해유!!~',
-        },
-    ];
+    const [comment, setComment] = useState('');
+    const setDiary = useSetRecoilState(diaryState);
+
+    const submitComment = (e) => {
+        e.preventDefault();
+        console.log('댓글: ' + comment);
+        if (comment !== '') {
+            diaryService.postComment(props.data.diaryId, comment).then((res) => {
+                if (res) {
+                    alert('댓글이 저장되었습니다.');
+                    setComment('');
+                    setDiary((prev) => {
+                        const tmp = [...prev];
+                        return tmp.map((diary) => {
+                            if (diary.diaryId === props.data.diaryId) {
+                                console.log('댓글 추가');
+                                return { ...diary, comments: res.data };
+                            }
+                        });
+                    });
+                } else {
+                    alert('댓글 저장에 실패했습니다.');
+                }
+            });
+        }
+    };
+    const handleComment = (e) => {
+        setComment(e.target.value);
+    };
+    const onKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            submitComment(e);
+        }
+    };
+
     return (
         <>
-            <Stack spacing={1}>
-                {comments.map((comment) => (
-                    //props.data.comments.map((comment) => (
-                    <Box
-                        key={comment.id}
-                        sx={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'row' }}>
-                        <Avatar sx={{ width: 40, height: 40, mr: 1 }} />
+            {props.data.comments && props.data.comments.length > 0 && (
+                <Stack spacing={1}>
+                    {props.data.comments.map((comment) => (
                         <Box
+                            key={comment.commentId}
                             sx={{
-                                backgroundColor: 'quaternary.main',
-                                borderRadius: 2,
-                                px: 2,
-                                py: 1,
+                                display: 'flex',
+                                alignItems: 'flex-start',
+                                flexDirection: 'row',
                             }}>
-                            <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
-                                {comment.username}
-                            </Typography>
-                            <Typography variant="body1">{comment.content}</Typography>
+                            <Avatar sx={{ width: 40, height: 40, mr: 1 }} />
+                            <Box
+                                sx={{
+                                    backgroundColor: 'quaternary.main',
+                                    borderRadius: 2,
+                                    px: 2,
+                                    py: 1,
+                                }}>
+                                <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
+                                    {comment.nickname}
+                                </Typography>
+                                <Typography variant="body1">{comment.contents}</Typography>
+                            </Box>
                         </Box>
-                    </Box>
-                ))}
-            </Stack>
+                    ))}
+                </Stack>
+            )}
             <Box
                 component="form"
                 sx={{
@@ -51,9 +83,15 @@ export default function Comments(props) {
                 <InputBase
                     sx={{ ml: 1, flex: 1 }}
                     placeholder="댓글을 입력해주세요..."
-                    inputProps={{ 'aria-label': 'search google maps' }}
+                    inputProps={{ 'aria-label': '댓글 입력' }}
+                    onChange={handleComment}
+                    onKeyDown={onKeyPress}
                 />
-                <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <IconButton
+                    type="button"
+                    sx={{ p: '10px' }}
+                    aria-label="search"
+                    onClick={submitComment}>
                     <SendIcon />
                 </IconButton>
             </Box>
