b1e6a18ebaace9aa5b1a1874ee9955b935b6452a
charmeee <mandugohigh@gmail.com>
2023-02-23 22:00:18 +0900

    feat(api): 방명록 api 추가 및 적용

---
+++ src/services/guestBook_api.js
@@ -0,0 +1,29 @@
+import client from './http_client';
+
+class GuestBook {
+    getGuestBook = async (id) => {
+        try {
+            const response = await client.get(`guestBook/${id}`);
+            console.log('getGuestBook', response.data);
+            return Array.isArray(response.data) ? response.data : null;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    };
+
+    postGuestBook = async (hostId, content) => {
+        ///api/guestBook/{hostMemberId}
+        try {
+            const response = await client.post(`guestBook/${hostId}`, content);
+            console.log('postGuestBook', response.data);
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    };
+}
+
+const guestBookService = new GuestBook();
+export default guestBookService;
