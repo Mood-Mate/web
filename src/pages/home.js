4762171bac3c90ceef238d595db9d5eb5529ce8a
charmeee <mandugohigh@gmail.com>
2023-03-06 19:21:28 +0900

    feat: 홈 페이지 다시 제작. - 프로필 제작 - 방명록 제작 - 팔로워들의 글을 볼 수 있는 컴포넌트 제작

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,45 +1,88 @@
 import AppBar from '../components/header';
-import Guestbook from '../components/Home/guestbook';
-import Topbutton from '../components/Home/topbutton';
-import Diary from '../components/Home/diary';
-import TopProfile from '../components/Home/topProfile';
-import TopGuestbook from '../components/Home/topGuestbook';
-import { useMediaQuery } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, Typography } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Post from '../components/Profile/post';
+import { useRecoilValue } from 'recoil';
+import GuestBook from '../components/Profile/guestBook';
+import { useEffect, useState } from 'react';
+import diaryService from '../services/diary_api';
+import { userState } from '../atom/auth';
+import HomeProfile from '../components/Home/homeProfile';
 
 export default function Home() {
-    const isMobile = useMediaQuery('(max-width: 600px)');
+    const isMobile = useMediaQuery('(max-width: 700px)');
+    const [diaryData, setDiaryData] = useState({ data: [], next: null });
+    const user = useRecoilValue(userState);
+
+    useEffect(() => {
+        diaryService.getFolloweeDiary(diaryData.next).then((res) => {
+            if (res?.data) {
+                setDiaryData((diary) => {
+                    const newData = [...diary.data, ...res.data.data];
+                    const filteredData = newData.reduce(function (acc, current) {
+                        if (acc.findIndex(({ diaryId }) => diaryId === current.diaryId) === -1) {
+                            acc.push(current);
+                        }
+                        return acc;
+                    }, []);
+                    return {
+                        data: filteredData,
+                        next: res.data.next,
+                    };
+                });
+            } else {
+                alert('일기를 불러오는데 실패했습니다.');
+            }
+        });
+    }, []);
+
     const boxStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
         borderRadius: 2,
         border: 2,
         borderColor: 'primary.main',
-        backgroundColor: 'background.default',
-        width: '80%',
-        margin: 5,
         padding: 2,
+        margin: 2,
+        backgroundColor: 'background.box',
     };
     return (
-        <>
+        <div className="profile">
             <AppBar />
-            <Box sx={{ margin: 2 }}>
-                <Box sx={{ display: 'flex', justifyContent: 'end' }}>
-                    <Topbutton></Topbutton>
-                </Box>
-            </Box>
+            <HomeProfile userId={user.id} style={boxStyle} />
 
-            <Box sx={{ display: 'flex', flexDirection: isMobile ? 'column' : 'row' }}>
-                <Box sx={{ ...boxStyle }}>
-                    <TopProfile />
-                    <Diary></Diary>
+            <Box
+                sx={{
+                    margin: 'auto',
+                    display: 'flex',
+                    flexDirection: isMobile ? 'column' : 'row',
+                    justifyContent: 'center',
+                    alignItems: 'flex-start',
+                    padding: 1,
+                }}>
+                <Box sx={{ width: isMobile ? '100%' : 350, minWidth: 350 }}>
+                    <GuestBook style={boxStyle} userId={user.id} />
                 </Box>
-                <Box sx={{ ...boxStyle }}>
-                    <TopGuestbook />
-                    <Guestbook></Guestbook>
+
+                <Box sx={{ maxWidth: 760, minWidth: 350, width: '100%' }}>
+                    {diaryData.data.length > 0 ? (
+                        diaryData.data.map((data) => (
+                            <Post
+                                key={data['diaryId']}
+                                data={data}
+                                style={boxStyle}
+                                isFollowee={true}
+                            />
+                        ))
+                    ) : (
+                        <Typography
+                            variant="h5"
+                            align={'center'}
+                            sx={{ paddingY: '30vh', whiteSpace: 'pre-line' }}
+                            gutterBottom>
+                            {'등록된 글이 없습니다.\n글을 등록해보세요.'}
+                        </Typography>
+                    )}
                 </Box>
             </Box>
-        </>
+        </div>
     );
 }
