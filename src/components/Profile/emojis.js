518cda8309d1d9806f52e2baa3ef317dd0f366d2
charmeee <mandugohigh@gmail.com>
2023-03-11 05:40:30 +0900

    feat: 감정표현 이모지 버튼 svg파일 추가.

--- src/components/Profile/emojis.js
+++ src/components/Profile/emojis.js
@@ -1,19 +1,29 @@
-import { Box, Divider, IconButton } from '@mui/material';
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import { Box, Divider, IconButton, SvgIcon } from '@mui/material';
+import { ReactComponent as AngryIcon } from 'assets/emoji_Icon/angry.svg';
+import { ReactComponent as CryIcon } from 'assets/emoji_Icon/cry.svg';
+import { ReactComponent as LoveIcon } from 'assets/emoji_Icon/love.svg';
+import { ReactComponent as StupidIcon } from 'assets/emoji_Icon/stupid.svg';
 
+//
 export default function Emojis() {
+    const handleEmojiClick = (e) => {
+        console.log(e.currentTarget.id);
+    };
     return (
         <Box sx={{ py: 1 }}>
             <Divider />
-            <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                <IconButton>
-                    <SentimentSatisfiedAltIcon />
+            <Box sx={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                <IconButton id="love" onClick={handleEmojiClick}>
+                    <LoveIcon style={{ height: 25, width: 25 }} />
                 </IconButton>
-                <IconButton>
-                    <SentimentSatisfiedAltIcon />
+                <IconButton id="sad" onClick={handleEmojiClick}>
+                    <CryIcon style={{ height: 25, width: 25 }} />
                 </IconButton>
-                <IconButton>
-                    <SentimentSatisfiedAltIcon />
+                <IconButton id="angry" onClick={handleEmojiClick}>
+                    <AngryIcon style={{ height: 25, width: 25 }} />
+                </IconButton>
+                <IconButton id="stupid" onClick={handleEmojiClick}>
+                    <StupidIcon style={{ height: 25, width: 25 }} />
                 </IconButton>
             </Box>
             <Divider />
