b0ef65386e8b9d05a1947365c4cd5a5a12187862
charmeee <mandugohigh@gmail.com>
2023-03-20 06:05:29 +0900

    feat: 프로필 박스에서 팔로우 언팔로우 기능 추가 - ui 추가 - api 연결

--- src/components/Profile/profileBox.js
+++ src/components/Profile/profileBox.js
@@ -1,13 +1,39 @@
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { useEffect, useState } from 'react';
 import authService from '../../services/auth_api';
 import UserImage from '../Common/userImage';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../atom/auth';
+import followService from '../../services/follow_api';
 
 export default function ProfileBox(props) {
+    const rootUser = useRecoilValue(userState);
+    const [isFollowing, setIsFollowing] = useState(false);
     const [user, setUser] = useState(null);
+    const handleFollow = () => {
+        if (isFollowing) {
+            followService.unfollow(props.userId).then((res) => {
+                if (res) {
+                    setIsFollowing(false);
+                } else {
+                    alert('언팔로우에 실패했습니다.');
+                }
+            });
+        } else {
+            followService.follow(props.userId).then((res) => {
+                if (res) {
+                    setIsFollowing(true);
+                } else {
+                    alert('팔로우에 실패했습니다.');
+                }
+            });
+        }
+    };
     useEffect(() => {
-        if (props.userId) {
+        if (props.userId && rootUser.id) {
+            console.log('props.userId', props.userId);
+            console.log('rootUser.id', rootUser.id);
             authService.getUserInfoById(props.userId).then((res) => {
                 if (res) {
                     console.log(res);
@@ -16,8 +42,18 @@ export default function ProfileBox(props) {
                     alert('유저 정보를 불러오는데 실패했습니다.');
                 }
             });
+            if (rootUser.id != props.userId) {
+                followService.getFollowing(rootUser.id).then((res) => {
+                    if (res) {
+                        res.data.some((e) => e.followingMemberId == props.userId) &&
+                            setIsFollowing(true);
+                    } else {
+                        alert('팔로잉 정보를 불러오는데 실패했습니다.');
+                    }
+                });
+            }
         }
-    }, [props.userId]);
+    }, [props.userId, rootUser.id]);
     return (
         user && (
             <>
@@ -26,7 +62,7 @@ export default function ProfileBox(props) {
                         display: 'flex',
                         flexDirection: 'row',
                         justifyContent: 'space-between',
-                        alignItems: 'center',
+                        alignItems: 'flex-start',
                         textAlign: 'center',
                     }}>
                     <UserImage userId={props.userId} width={80} profileImage={user.picture} />
@@ -40,9 +76,24 @@ export default function ProfileBox(props) {
                             <Box sx={{ width: 100 }}>{user.followerCount + '명'}</Box>
                             <Box sx={{ width: 100 }}>{user.followingCount + '명'}</Box>
                         </Box>
+                        {rootUser.id != props.userId && (
+                            <Box sx={{ width: '100%', textAlign: 'center', marginTop: 1 }}>
+                                <Button
+                                    sx={{
+                                        margin: 'auto',
+                                        backgroundColor: isFollowing ? 'grey' : '#a8d59e99',
+                                        paddingY: 0.2,
+                                    }}
+                                    onClick={handleFollow}
+                                    variant="contained">
+                                    {isFollowing ? '언팔로잉' : '팔로잉'}
+                                </Button>
+                            </Box>
+                        )}
                     </Box>
                 </Box>
-                <Typography sx={{ paddingY: 3 }}>{user.introduce}</Typography>
+
+                {user.introduce && <Typography sx={{ paddingY: 3 }}>{user.introduce}</Typography>}
             </>
         )
     );
