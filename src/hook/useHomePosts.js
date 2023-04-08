b07651d4a1368da61b58b17f36e1afdf1cdb523a
charmeee <mandugohigh@gmail.com>
2023-04-09 05:22:55 +0900

    fix:  불필요하다고 생각되는 useCallback제거

--- src/hook/useHomePosts.js
+++ src/hook/useHomePosts.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import diaryService from '../services/diary_api';
 
 const useHomePosts = (nextPage) => {
@@ -6,38 +6,41 @@ const useHomePosts = (nextPage) => {
     const [next, setNext] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-    const sendQuery = useCallback(async () => {
-        // console.log('sendQueryStart', nextPage, diaryList);
+    const sendQuery = () => {
+        console.log('sendQueryStart', nextPage, diaryList);
         setError(null);
-        try {
-            setLoading(true);
-            const res = await diaryService.getFollowingDiary(nextPage);
-            if (res?.data.data) {
-                setDiaryList((prev) => {
-                    let list = [...prev, ...res.data.data];
-                    const filteredData = list.reduce(function (acc, current) {
-                        if (
-                            acc.findIndex(
-                                (e) => parseInt(e.diaryId) === parseInt(current.diaryId),
-                            ) === -1
-                        ) {
-                            acc.push(current);
-                        }
-                        return acc;
-                    }, []);
-                    list = filteredData;
-                    // console.log('sendQueryEnd', nextPage, list);
-                    return list;
-                });
-                setNext(res.data.next);
-            } else {
-                setError('일기를 불러오는데 실패했습니다.');
-            }
-        } catch (e) {
-            setError(e);
-        }
-        setLoading(false);
-    }, [nextPage]);
+        setLoading(true);
+        diaryService
+            .getFollowingDiary(nextPage)
+            .then((res) => {
+                if (res?.data.data) {
+                    setDiaryList((prev) => {
+                        let list = [...prev, ...res.data.data];
+                        const filteredData = list.reduce(function (acc, current) {
+                            if (
+                                acc.findIndex(
+                                    (e) => parseInt(e.diaryId) === parseInt(current.diaryId),
+                                ) === -1
+                            ) {
+                                acc.push(current);
+                            }
+                            return acc;
+                        }, []);
+                        list = filteredData;
+                        // console.log('sendQueryEnd', nextPage, list);
+                        return list;
+                    });
+                    setNext(res.data.next);
+                } else {
+                    setError('일기를 불러오는데 실패했습니다.');
+                }
+                setLoading(false);
+            })
+            .catch((e) => {
+                setError(e);
+                setLoading(false);
+            });
+    };
 
     useEffect(() => {
         console.log('useEffect', nextPage);
