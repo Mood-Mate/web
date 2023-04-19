7229326769576ccb2678ce9f984753891c3e2cc1
khc <nightletter65@gmail.com>
2023-04-19 22:02:53 +0900

    fix:oauth 임시

--- src/pages/oauth.js
+++ src/pages/oauth.js
@@ -1,22 +1,59 @@
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import oauthService from '../services/oauth_api';
+import axios from "axios";
 
 export default function OauthPage() {
     const { provider } = useParams();
     const [searchParams] = useSearchParams();
-
     const navigate = useNavigate();
 
-    useEffect(() => {
+    const tokenRequest = async () => {
         let authToken = searchParams.get('code');
-        console.log('authToken', authToken);
+
+        return await axios.post('https://kauth.kakao.com/oauth/token', {
+            grant_type : 'authorization_code',
+            client_id : 'b7e4a310abc54328e29fd9a6a2ff066c',
+            redirect_uri : 'http://localhost:3000/login/oauth2/code/kakao',
+            code : authToken
+        }, {
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            }
+        }).then(result => {
+            console.log(result);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    useEffect(() => {
+
+        // let access;
+        //
+        // const response = axios.post('https://kauth.kakao.com/oauth/token', {
+        //     grant_type : 'authorization_code',
+        //     client_id : 'b7e4a310abc54328e29fd9a6a2ff066c',
+        //     redirect_uri : 'http://localhost:3000/login/oauth2/code/kakao',
+        //     code : authToken
+        // }, {
+        //     headers: {
+        //         'content-type': 'application/x-www-form-urlencoded'
+        //     }
+        // }).then(result => {
+        //     access = result.data.access_token;
+        // }).catch(error => {
+        //     console.log(error)
+        // })
+        //
+        // console.log(access);
+
         switch (provider) {
             case 'google':
                 break;
             case 'kakao':
                 oauthService
-                    .kakaoLogin(authToken)
+                    .kakaoLogin()
                     .then((res) => {
                         console.log('kakao', res);
                     })
