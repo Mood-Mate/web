48340567dcf4243ac418514f145fe7a17fdf143a
charmeee <mandugohigh@gmail.com>
2023-05-09 19:31:17 +0900

    fix:  oauth

--- src/pages/oauth.js
+++ src/pages/oauth.js
@@ -1,65 +1,36 @@
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import oauthService from '../services/oauth_api';
-import axios from "axios";
+import axios from 'axios';
 
 export default function OauthPage() {
     const { provider } = useParams();
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
-
-    const tokenRequest = async () => {
-        let authToken = searchParams.get('code');
-
-        return await axios.post('https://kauth.kakao.com/oauth/token', {
-            grant_type : 'authorization_code',
-            client_id : 'b7e4a310abc54328e29fd9a6a2ff066c',
-            redirect_uri : 'http://localhost:3000/login/oauth2/code/kakao',
-            code : authToken
-        }, {
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded'
-            }
-        }).then(result => {
-            console.log(result);
-        }).catch(error => {
-            console.log(error);
-        })
-    }
+    const authToken = searchParams.get('code');
 
     useEffect(() => {
-
-        // let access;
-        //
-        // const response = axios.post('https://kauth.kakao.com/oauth/token', {
-        //     grant_type : 'authorization_code',
-        //     client_id : 'b7e4a310abc54328e29fd9a6a2ff066c',
-        //     redirect_uri : 'http://localhost:3000/login/oauth2/code/kakao',
-        //     code : authToken
-        // }, {
-        //     headers: {
-        //         'content-type': 'application/x-www-form-urlencoded'
-        //     }
-        // }).then(result => {
-        //     access = result.data.access_token;
-        // }).catch(error => {
-        //     console.log(error)
-        // })
-        //
-        // console.log(access);
-
         switch (provider) {
             case 'google':
-                break;
-            case 'kakao':
                 oauthService
-                    .kakaoLogin()
+                    .googleLogin(authToken)
                     .then((res) => {
-                        console.log('kakao', res);
+                        console.log('google', res);
                     })
                     .catch((err) => {
-                        console.log('kakaoError', err);
+                        console.log('googleError', err);
                     });
+                break;
+            case 'kakao':
+                console.log(authToken);
+                // oauthService
+                //     .kakaoLogin(authToken)
+                //     .then((res) => {
+                //         console.log('kakao', res);
+                //     })
+                //     .catch((err) => {
+                //         console.log('kakaoError', err);
+                //     });
                 break;
             case 'naver':
                 //request to naver
--- src/services/oauth_api.js
+++ src/services/oauth_api.js
@@ -3,12 +3,22 @@ import client from './http_client';
 class Oauth {
     kakaoLogin = async (token) => {
         try {
-            const response = await client.post('oauth', {
+            const response = await client.post('/oauth2/authorization/kakao', {
                 provider: 'KAKAO',
                 accessToken: token,
             });
         } catch (error) {
             console.log('kakaoLogin', error);
+        }
+    };
+    googleLogin = async (token) => {
+        try {
+            const response = await client.post('/oauth2/authorization/google', {
+                provider: 'GOOGLE',
+                accessToken: token,
+            });
+        } catch (error) {
+            console.log('googleLogin', error);
         }
     };
 }
