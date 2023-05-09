92febf39a3899976032d0d82ef52f936c922e4c8
charmeee <mandugohigh@gmail.com>
2023-05-09 22:27:12 +0900

    feat: oauth 기능 완료

--- src/pages/oauth.js
+++ src/pages/oauth.js
@@ -2,43 +2,51 @@ import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import oauthService from '../services/oauth_api';
 import axios from 'axios';
+import cookie from 'react-cookies';
+import authService from '../services/auth_api';
+import { useSetRecoilState } from 'recoil';
+import { userState } from '../atom/auth';
 
 export default function OauthPage() {
     const { provider } = useParams();
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
-    const authToken = searchParams.get('code');
+    const authToken = searchParams.get('access_token');
+    const setUser = useSetRecoilState(userState);
 
-    useEffect(() => {
-        switch (provider) {
-            case 'google':
-                oauthService
-                    .googleLogin(authToken)
-                    .then((res) => {
-                        console.log('google', res);
-                    })
-                    .catch((err) => {
-                        console.log('googleError', err);
-                    });
-                break;
-            case 'kakao':
-                console.log(authToken);
-                // oauthService
-                //     .kakaoLogin(authToken)
-                //     .then((res) => {
-                //         console.log('kakao', res);
-                //     })
-                //     .catch((err) => {
-                //         console.log('kakaoError', err);
-                //     });
-                break;
-            case 'naver':
-                //request to naver
-                break;
-            default:
-                navigate('/login');
+    useEffect(async () => {
+        if (authToken) {
+            const expires = new Date();
+            expires.setDate(expires.getDate() + 7); //7일동안 쿠키 유지
+            authService.getUser(authToken).then((response) => {
+                if (response) {
+                    handleResponse(provider, response);
+                }
+            });
         }
     }, [provider]);
+
+    const handleResponse = (vendor, response) => {
+        if (response?.data) {
+            setUser({
+                isLogin: true,
+                vendor,
+                id: response.data['memberId'],
+                name: response.data['name'],
+                nickname: response.data['nickname'],
+                introduce: response.data['introduce'],
+                picture: response.data['picture'],
+                email: response.data['email'],
+                dateOfBirth: response.data['dateOfBirth'],
+                gender: response.data['gender'],
+            });
+            console.log('로그인 완료');
+            navigate('/');
+        } else {
+            alert('로그인 실패');
+        }
+    };
+
     return (
         <div>
             <h1>소셜 로그인 로딩 중</h1>
